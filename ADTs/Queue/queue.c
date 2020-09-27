1a48f06b6abb5ad90379e85916483599e6259117
Philipe A. Marques <lipe.aguiar.marques@gmail.com>
2020-09-27 13:25:20 -0300

    Added heterogeneous queue implementation to ADTs

---
+++ ADTs/Queue/queue.c
@@ -0,0 +1,78 @@
+#include "queue.h"
+
+struct item {
+  void *element;
+  struct item* next;
+};
+
+struct queue {
+  Item* head;
+  Item* tail;
+  int length;
+};
+
+Queue *createQueue() {
+  Queue *queue = malloc(sizeof(Queue));
+  queue->head = NULL;
+  queue->tail = NULL;
+  queue->length = 0;
+  return queue;
+}
+
+void enqueue(Queue *queue, void *element) {
+  if (queue->head == NULL) {
+    Item *item = malloc(sizeof(Item));
+    item->next = NULL;
+    item->element = element;
+    queue->head = item;
+    queue->tail = queue->head;
+    queue->length += 1;
+  } else {
+    Item *item = malloc(sizeof(Item));
+    item->next = NULL;
+    item->element = element;
+    queue->tail->next = item;
+    queue->tail = item;
+    queue->length += 1;
+  }
+}
+
+Item* dequeue(Queue *queue) {
+  if (queue->head != NULL) {
+    Item *item = queue->head;
+    queue->head = queue->head->next;
+    queue->length -= 1;
+    item->next = NULL;
+    return item;
+  }
+}
+
+void deleteQueue(Queue *queue) {
+  Item *p = queue->head;
+  for (int i = 0; i < queue->length; i++) {
+    queue->head = queue->head->next;
+    free(p);
+    p = queue->head;
+  }
+  free(queue);
+}
+
+void printQueue(Queue *queue, int type) {
+  Item *p = queue->head;
+  if (type == 1) {
+    for (int i = 0; i < queue->length; i++) {
+      int *value = p->element;
+      printf("%d ", *value);
+      p = p->next;
+    }
+    putchar('\n');
+  }
+}
+
+void* getElement(Item *item) {
+  return item->element;
+}
+
+int queueLength(Queue *queue) {
+  return queue->length;
+}
